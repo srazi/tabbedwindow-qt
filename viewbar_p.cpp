3572bd4f2ca8834e692946aa075b0a9eca45db46
Daniele Esposti <expo@expobrain.net>
2012-07-09 00:12:41 +0100

    Fixed click event on a tab

--- viewbar_p.cpp
+++ viewbar_p.cpp
@@ -4,22 +4,35 @@
 
 ViewBarPrivate::ViewBarPrivate(QWidget *parent) :
     QTabBar(parent),
-    dragStartPos(QPoint()),
     dragging(-1)
 {
 }
 
 void ViewBarPrivate::mousePressEvent(QMouseEvent *event)
 {
+    // If left button is pressed save current mouse position for
+    // possible draggin action triggered in the future
     if (event->button() == Qt::LeftButton) {
         qDebug() << "left button pressed at" << event->pos();
         dragStartPos = event->pos();
     }
+
+    // Call superclass
+    QTabBar::mousePressEvent(event);
 }
 
 
 void ViewBarPrivate::mouseReleaseEvent(QMouseEvent *event) {
-    if (dragging > -1 & (event->button() == Qt::LeftButton)) {
+    // Call superclass if a button different than the left one was released
+    // and return
+    if (event->button() != Qt::LeftButton) {
+        QTabBar::mouseReleaseEvent(event);
+        return;
+    }
+
+    // If left button was released and a dragging action is involved
+    // stop dragging
+    if (dragging > -1) {
         qDebug() << "stop dragging tab" << dragging;
         dragging = -1;
     }
