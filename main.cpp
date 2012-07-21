bcd8eff032903b92e41b0398814b4a80a5be7e9e
Daniele Esposti <expo@expobrain.net>
2012-07-22 00:15:49 +0100

    Code prettify

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <QApplication>
 #include <QToolBar>
+#include <QMenuBar>
 
 #include "tabbedwindow.h"
 
 
 int main(int argc, char *argv[])
 {
+    // Create application
     QApplication a(argc, argv);
+
+    // Create tabbed views
     TabbedWindow w;
     QMainWindow red, green, blue;
 
@@ -14,14 +18,23 @@ int main(int argc, char *argv[])
     blue.setStyleSheet("QMainWindow { background-color: blue; }");
     green.setStyleSheet("QMainWindow { background-color: green; }");
 
-    QToolBar *tb = red.addToolBar("toolbar");
-    tb->addAction("RED");
-
     w.addView(&red, QString("Red View"));
     w.addView(&blue, QString("Blue view"));
     w.addView(&green, QString("Green view"));
+
+    // Added test toolbar
+    QToolBar *tb = red.addToolBar("toolbar");
+    tb->addAction("RED");
+
+    // Add test menubar
+    QAction *ma = new QAction("Test", &green);
+    QMenu *m = green.menuBar()->addMenu("File");
+    m->addAction(ma);
+
+    // Set size and show
     w.resize(400, 400);
     w.show();
     
+    // Start mainloop
     return a.exec();
 }
