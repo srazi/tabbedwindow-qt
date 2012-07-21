6a54eca45ef12e963ae6a96046569c2e0c010fa5
Daniele Esposti <expo@expobrain.net>
2012-07-21 23:56:05 +0100

    Add QMainWindow instances instead of QWidgets

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <QApplication>
+#include <QToolBar>
+
 #include "tabbedwindow.h"
 
 
@@ -6,18 +8,20 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     TabbedWindow w;
-    w.resize(400, 400);
-    w.show();
+    QMainWindow red, green, blue;
 
-    QWidget red, green, blue;
+    red.setStyleSheet("QMainWindow { background-color: red; }");
+    blue.setStyleSheet("QMainWindow { background-color: blue; }");
+    green.setStyleSheet("QMainWindow { background-color: green; }");
 
-    red.setStyleSheet("background-color: red");
-    blue.setStyleSheet("background-color: blue");
-    green.setStyleSheet("background-color: green");
+    QToolBar *tb = red.addToolBar("toolbar");
+    tb->addAction("RED");
 
     w.addView(&red, QString("Red View"));
     w.addView(&blue, QString("Blue view"));
     w.addView(&green, QString("Green view"));
+    w.resize(400, 400);
+    w.show();
     
     return a.exec();
 }
